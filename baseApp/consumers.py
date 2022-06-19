24e9124722f5e08f3c9e93ab9a50a2d01245dc99
Hamza-Sarwar <hamzasarwar129@gmail.com>
2022-06-19 22:09:57 +0500

    All Coinbase Currencies has been displayed at web page

--- baseApp/consumers.py
+++ baseApp/consumers.py
@@ -1,6 +1,6 @@
 import json
+from tarfile import RECORDSIZE
 from time import sleep
-from random import randint
 from websocket import create_connection
 from channels.generic.websocket import WebsocketConsumer
 
@@ -10,37 +10,31 @@ class WSConsumer(WebsocketConsumer):
 
         URL = "wss://ws-feed.pro.coinbase.com"
         ws = create_connection(URL)
-
         params = {
             "type": "subscribe",
-            "product_ids": ["BTC-USD", "ETH-USD"],
+            "product_ids": ["BTC-USD", "ETH-USD", "ADA-USD", "SOL-USD", "DOT-USD"],
             "channels": ["ticker"]
             }
-
         ws.send(json.dumps(params))
         while True:
             result = ws.recv()
             sleep(1)
-            if (result.find("ETH-USD") != -1):
-
-                print(result)
+            record = result.split(',')[2:8]
+            
+            if len(record) != 6:
+                del record
             else:
-                print("Not here")
-            # record = result.split(',')[3:8]
-            # if len(record) != 5:
-            #     del record
-            # else:
+                #print(f"{record} size:{len(record)} ")
+                eth = {
+                'type': record[0][14:-1],
+                'price': record[1][9:-1],
+                'open_24h': record[2][12:-1],
+                'volume_24h': record[3][14:-1],
+                'low_24h': record[4][11:-1],
+                'high_24h': record[5][12:-1]
+                }
+                self.send(json.dumps(eth))
 
-            #     print(f"{record} size:{len(record)} ")
-                # self.send(json.dumps({
-                #     'price': record[0][9:-1],
-                #     'open_24h': record[1][12:-1],
-                #     'volume_24h': record[2][14:-1],
-                #     'low_24h': record[3][11:-1],
-                #     'high_24h': record[4][12:-1]
-                # }))
+                   
 
-        # for i in range(1000):
-        #     self.send(json.dumps({'message': randint(1,1000)}))
-        #     sleep(1)
 
